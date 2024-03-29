bfc7ce511a387adbfb4d186eef179b3b32e10646
Azalurg <91213389+Azalurg@users.noreply.github.com>
2024-03-29 10:18:41 +0100

    Linear Regression realtime deom

--- real_time_virtualization/02_Logistic_Regression.py
+++ real_time_virtualization/02_Logistic_Regression.py
@@ -11,23 +11,30 @@ class InteractivePlot:
         self.fig.set_figheight(4.5)
         self.fig.set_figwidth(8)
         self.ax.set_xlim(0, 100)
-        self.ax.set_ylim(0, 100)
+        self.ax.set_ylim(-0.1, 1.1)
 
         self.main_line, = self.ax.plot([], [], "oc", picker=True, pickradius=5)
-        self.regression_line, = self.ax.plot([], [], color="black", alpha=0.7, linestyle="-")
+        self.regression_line, = self.ax.plot([], [], color="black", alpha=0.7, linestyle=":")
 
         self.fig.canvas.mpl_connect('button_press_event', self.on_click)
         self.fig.canvas.mpl_connect('pick_event', self.on_pick)
 
+    @staticmethod
+    def logistic(x, x0, k, L):
+        return L / (1 + np.exp(-k * (x - x0)))
+
     def add_new_point(self, x, y):
         self.xdata.append(x)
-        self.ydata.append(y)
+        self.ydata.append(round(y))
+
+        x0 = max(int(np.sum(np.multiply(self.xdata, self.ydata)) / np.sum(self.xdata) * 100) - 13, 0)
+        k = 0.21
+
+        logic_x = np.arange(0, 100, 0.5)
+        l = self.logistic(logic_x, x0, k, L=1)
+
         self.main_line.set_data(self.xdata, self.ydata)
-
-        coef = np.polyfit(self.xdata, self.ydata, 1)
-        poly1d = np.poly1d(coef)(self.xdata)
-
-        self.regression_line.set_data(self.xdata, poly1d)
+        self.regression_line.set_data(logic_x, l)
 
         self.main_line.figure.canvas.draw()
         self.regression_line.figure.canvas.draw()
